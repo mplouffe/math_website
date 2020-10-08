8c2c38b9e4e037ae8d1076a4329a6b3340718a1b
Matheu Plouffe <matheuplouffe@gmail.com>
2020-10-07 20:31:11 -0500

    Added trig functions.

--- public/js/unitCircle.js
+++ public/js/unitCircle.js
@@ -75,6 +75,42 @@ var displayPythagorasOnUnitCircle = function(){
     context.fillStyle = "#000";
     context.fillText("x", 128 + (64*Math.cos(Math.PI/4))/2, 128 + 10);
     context.fillText("y", 128 + (64*Math.cos(Math.PI/4)) + 5, 128 - (64*Math.cos(Math.PI/4))/2 + 10);
+    context.fillText("Θ", 128+18, 128-3);
+}
+
+var displayTrigFunctions = function(){
+    let canvas = document.getElementById("unitCircleViz_04");
+    canvas.width = 256;
+    canvas.height = 256;
+    let context = canvas.getContext('2d');
+
+    context.fillStyle = "#3CF";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.beginPath();
+    // y axis
+    context.moveTo(128,0);
+    context.lineTo(128,256);
+    // x axis
+    context.moveTo(0,128);
+    context.lineTo(256,128);
+    // unit circle
+    context.moveTo(128+64, 128);
+    context.arc(128,128,64,0, 2*Math.PI);
+
+    context.moveTo(128,128);
+    context.lineTo(128+(64*Math.cos(Math.PI/4)), 128-(64*Math.sin(Math.PI/4)));
+    context.arc(128+(64*Math.cos(Math.PI/4)), 128-(64*Math.sin(Math.PI/4)), 2, 0, 2 * Math.PI, true);;
+    // context.lineTo(128+(64*Math.cos(Math.PI/4)), 128);
+
+    context.moveTo(128, 128);
+    context.arc(128, 128, 15, -Math.PI/4, 0);
+    context.stroke();
+
+    context.font = "11px Consolas";
+    context.fillStyle = "#000";
+    context.fillText("(Cos(Θ), Sin(Θ))", 128 + (64*Math.cos(Math.PI/4)) + 2, 128-(64*Math.sin(Math.PI/4)) - 5);
+    context.fillText("Θ", 128+18, 128-3);
 }
 
 var displayPythagoras = function(){
@@ -110,5 +146,6 @@ var displayPythagoras = function(){
 window.addEventListener('DOMContentLoaded', (event) => {
     displayUnitCircle();
     displayPythagoras();
-    displayPythagorasOnUnitCircle();    
+    displayPythagorasOnUnitCircle();
+    displayTrigFunctions();   
 });
